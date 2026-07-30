4f681e5d1487b48701e48cbfcb9f9c7dfd23df54
Sravan Turibilli <sra1turibilli@gmail.com>
2026-07-30 16:29:28 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.go
@@ -0,0 +1,11 @@
+func isValid(s string) bool {
+	for strings.Contains(s, "()") || 
+	strings.Contains(s, "[]") || 
+	strings.Contains(s, "{}"){
+		s = strings.Replace(s, "()", "", -1)
+		s = strings.Replace(s, "[]", "", -1)
+		s = strings.Replace(s, "{}", "", -1)
+	}
+
+	return s==""
+}
