a0623a3e1490b864cf5444953259c042d166fb54
Sravan Turibilli <sra1turibilli@gmail.com>
2026-08-03 10:31:11 +0530

    Update: reverse-a-linked-list - submission-1

--- Data Structures & Algorithms/reverse-a-linked-list/submission-1.go
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.go
@@ -6,17 +6,39 @@
  * }
  */
 
-func reverseList(head *ListNode) *ListNode {
-	if head == nil{
-		return nil
+ /*
+ idea: 
+ - use a dummy node
+ - pointers at each of the list
+ - compare and attach the lower one to dummy
+ - move the pointer at that list
+ 
+ time-complexity: O(n+m)
+ space-complexity: O(1)
+ */
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+
+	curr := dummy
+	for list1 != nil && list2 != nil{
+		if list1.Val <= list2.Val{
+			curr.Next = list1
+			list1 = list1.Next
+		} else {
+			curr.Next = list2
+			list2 = list2.Next
+		}
+		curr = curr.Next
 	}
 
-	newHead := head
-	if head.Next!=nil{
-		newHead = reverseList(head.Next)
-		head.Next.Next = head
+	if list1 == nil{
+		curr.Next = list2
 	}
-	head.Next = nil
 
-	return newHead
+	if list2 == nil{
+		curr.Next = list1
+	}
+
+	return dummy.Next
 }
