334c83bd8bf7caf89e53eef46b7452cae2f50e91
Sravan Turibilli <sra1turibilli@gmail.com>
2026-08-15 13:23:45 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.go
@@ -0,0 +1,106 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+ /*
+ start: 12pm, 08-15-26
+
+ req:
+ 
+ edge: 
+ - ending soon
+ - EC: take care if a list ends
+
+ BF:
+ - map: head : node
+ - map: tail : node
+ - t: O(n*m)
+ - s: O(n*m)
+
+ BF-implement: 12:15pm
+
+	head := make(map[int]*ListNode)
+	tail := make(map[int]*ListNode)
+	for i:=0; i<len(lists); i++ {
+		node := lists[i]
+		for node != nil {
+			next := node.Next
+			node.Next = nil
+			t, ok := tail[node.Val]
+			if !ok {
+				tail[node.Val] = node
+				head[node.Val] = node
+			} else {
+				t.Next = node
+				tail[node.Val] = node
+			}
+
+			node = next
+		}
+	}
+
+	var keys []int
+	for k, v := range head {
+		fmt.Println(k, v)
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	dummyHead := ListNode{}
+	prev := &dummyHead
+	for i:=0; i<len(keys); i++ {
+		key := keys[i]
+		
+		prev.Next = head[key]
+		prev = tail[key]
+	}
+
+	return dummyHead.Next
+
+ */
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	head := make(map[int]*ListNode)
+	tail := make(map[int]*ListNode)
+	
+	for i:=0; i<len(lists); i++ {
+		node := lists[i]
+		for node != nil {
+			next := node.Next
+			node.Next = nil
+
+			t, ok := tail[node.Val]
+			if !ok {
+				tail[node.Val] = node
+				head[node.Val] = node
+			} else {
+				t.Next = node
+				tail[node.Val] = node
+			}
+
+			node = next
+		}
+	}
+
+	var keys []int
+	for k, _ := range head {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	dummyHead := ListNode{}
+	prev := &dummyHead
+	for i:=0; i<len(keys); i++ {
+		key := keys[i]
+
+		prev.Next = head[key]
+		prev = tail[key]
+	}
+	
+	return dummyHead.Next
+}
