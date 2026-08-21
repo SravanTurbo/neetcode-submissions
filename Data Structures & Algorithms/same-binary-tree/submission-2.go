8ac93f28e1934d76ac0a28bb176600644b8060bd
Sravan Turibilli <sra1turibilli@gmail.com>
2026-08-21 09:22:00 +0530

    Add: same-binary-tree - submission-2

---
+++ Data Structures & Algorithms/same-binary-tree/submission-2.go
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+/*
+Idea: recursive DFS.
+
+Trees are identical iff:
+- p, q are both nil 
+- or have the same non-nil value 
+- and their left/right subtrees are identical.
+
+Time: O(min(n, m)), where n and m are the number of nodes in the two trees.
+Space: O(h), where h is the maximum recursion depth.
+
+Trade-off: For a Skewed deep tree, recursion stack might overflow, might have to persist using iterative DFS/BFS
+*/
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+    if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
