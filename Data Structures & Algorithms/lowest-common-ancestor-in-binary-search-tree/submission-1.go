fdd1e35413087a79b690a33b1c79ff04852c4893
Sravan Turibilli <sra1turibilli@gmail.com>
2026-08-21 16:08:52 +0530

    Update: lowest-common-ancestor-in-binary-search-tree - submission-1

--- Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.go
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-1.go
@@ -8,43 +8,25 @@
  */
 
  /*
- Idea: check is subtree is sametree with root starting at all nodes.
+idea: BST val check
 
- Time: O(m*n), where m is substree nodes, n tree nodes
- Space: O(m+n), queue of len n for tree nodes and stack of len m for subtree
+Time: O(h) where h is the height of the tree
+Space: stack - O(h)
  */
 
-func isSameTree(p, q *TreeNode) bool {
-	if p == nil && q == nil {
-		return true
-	}
 
-	if p == nil || q == nil || p.Val != q.Val {
-		return false
-	}
+func lowestCommonAncestor(root *TreeNode, p *TreeNode, q *TreeNode) *TreeNode {
+    if root == nil || p == nil || q == nil {
+        return nil
+    }
 
-	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
-}
-
-func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
-    queue := []*TreeNode{root}
-
-	for len(queue) > 0 {
-		item := queue[0]
-		queue = queue[1:]
-
-		if isSameTree(item, subRoot) {
-			return true
-		}
-
-		if item.Left != nil {
-			queue = append(queue, item.Left)
-		}
-
-		if item.Right != nil {
-			queue = append(queue, item.Right)
-		}
-	}
-
-	return false
+    if (root.Val - p.Val) * (root.Val - q.Val) <= 0 {
+        return root
+    }
+    
+    if (p.Val < root.Val) {
+        return lowestCommonAncestor(root.Left, p, q)
+    } 
+    
+    return lowestCommonAncestor(root.Right, p, q)
 }
